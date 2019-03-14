db854c14ed5eb97f4f41b5cd94767b5788f6c9e1
pan <zhenjie.pan@watsons.com.cn>
2019-03-14 12:15:45 +0800

    build index.md

--- build.js
+++ build.js
@@ -1,25 +1,49 @@
 const fs = require('fs');
 const path = require('path');
 
-function getLinks() {
-    let files = fs.readdirSync('./docs');
-    console.log(files, '<--');
-    let links = files.map(file => {
-        let con = fs.readFileSync(path.join('./docs', file))
-        let firstLine = /^.*$/m.exec(con)[0];
-        return { url: `./docs/${file}`, text: firstLine };
+let docsDir = path.resolve(__dirname, './docs');
+
+async function getLinks() {
+    let files = fs.readdirSync(docsDir);
+    console.log('-----------> files:', files);
+    console.log();
+    console.log();
+
+    files = files.filter(file => {
+        let stat = fs.statSync(path.resolve(docsDir, file));
+        return !stat.isDirectory()
     });
+
+    let links = [];
+
+    for(let i = 0; i < files.length; i++) {
+        let link = await new Promise(resolve => {
+            let file = files[i];
+            let con = fs.readFileSync(path.join(docsDir, file))
+            let firstLine = /^.*$/m.exec(con)[0];
+            resolve({ url: `./docs/${file}`, text: firstLine });
+        });
+
+
+        links.push(link);
+    }
+
+    console.log('------------>links:', links)
+
     return links;
 }
 
-function write(fpath) {
-    let links = getLinks();
+async function write(fpath) {
+    let links = await getLinks();
+
     let linksMarkdown = links.map(link => {
         return `+ [${link.text}](${link.url})`;
     });
+
     let con = fs.readFileSync(fpath, 'utf8');
     const re = /(<!-- links -->)[\s\S]*/img;
     con = con.replace(re, '$1\r\n' + linksMarkdown.join('\r\n'));
+
 
     fs.writeFileSync(fpath, con, 'utf8');
     return true;
@@ -30,8 +54,10 @@ const child_process = require('child_process');
 try {
     write('index.md');
 
+    // index.md -> index.html
     let result = child_process.execSync('markdown-html index.md -o index.html');
     console.log('index.md -> index.html done..');
+    
 } catch (e) {
     console.log('err:', e);
 }
